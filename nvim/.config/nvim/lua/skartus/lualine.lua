6ce7c345312d4cc884a90087a6538b05ab692ea3
Leonardo Wiedemeier <leonardo.wiedemeier@protonmail.ch>
2023-05-06 15:27:00 +0200

    update xfce.sh

--- nvim/.config/nvim/lua/skartus/lualine.lua
+++ nvim/.config/nvim/lua/skartus/lualine.lua
@@ -1,5 +1,4 @@
 
-
 require('lualine').setup {
     options = {
         icons_enabled = true,
--- xfce.sh
+++ xfce.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 
 # terminal conf
-cp -r ./xfce4/terminal/ ~/.config/xfce4/terminal/
+cp -r ./xfce4/terminal ~/.config/xfce4/
 
 # keyboard shortcuts
-cp -r ./xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp ./xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 
